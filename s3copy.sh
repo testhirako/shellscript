65f3924af4f22ad0885a53515862fa11843d7272
testhirako <hirafukuoka2@gmail.com>
2019-04-07 23:59:08 +0900

    Update s3copy.sh

--- s3copy.sh
+++ s3copy.sh
@@ -3,6 +3,6 @@
 for i in `aws s3 ls s3://ns-137-test/23-test/ | awk '{print $2}'`
 do
    echo -2$i
-   #aws s3 cp --recursive s3://ns-137-test/23-test/$i s3/ns-137-test/23-test/$i_2
+   aws s3 cp --recursive s3://ns-137-test/23-test/$i s3/ns-137-test/23-test/$i_2
 done
 
